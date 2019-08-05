a4561c4cac31569748a671b299fa3b5bea8909b1
Chiranjibi Lawati <chiranjibilawati08@gmail.com>
2019-08-05 14:37:18 +0545

    completed

--- CalculatorLoan/app.js
+++ CalculatorLoan/app.js
@@ -1,10 +1,19 @@
 
 //listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+    //hide results
+    document.getElementById('results').style.display = 'none';
+    //show loader
+    document.getElementById('loading').style.display = 'block';
+
+    setTimeout(calculateResults, 2000);
+    
+    e.preventDefault();
+});
 
 
 //calculating results
-function calculateResults(e) {
+function calculateResults() {
 
   //UI variables
 
@@ -30,15 +39,24 @@ function calculateResults(e) {
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
-
+        //show result
+        document.getElementById('results').style.display = 'block';
+        //hide loader
+        document.getElementById('loading').style.display = 'none';
     }else {
         showError('Please check your numbers');
     }
-    e.preventDefault();
+    
 }
 
 //show error functions
 function showError(error) {
+
+
+       //show result
+       document.getElementById('results').style.display = 'none';
+       //hide loader
+       document.getElementById('loading').style.display = 'none';
     //create a div
 
     const errorDiv = document.createElement('div');
